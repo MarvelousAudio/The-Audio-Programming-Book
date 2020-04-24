04e56e2a12dbbe47e52569c50cdd73275cb2cf2f
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-04-23 20:28:06 -0700

    finished and it works

--- Hellotable/Hellotable/main.cpp
+++ Hellotable/Hellotable/main.cpp
@@ -121,18 +121,99 @@ void init()
 
 void cleanup()
 {
+    printf("cleaning up. . . ");
+#ifdef REALTIME //uses tiny audio library
+    tinyExit();
+#elif defined(BINARY_RAW_FILE)
+    fclose(file);
+#elif defined(WAVE_FILE) // uses portsf library
+    {
+        int err1, err2;
+        err1 = psf_sndClose(ofd);
+        err2 = psf_finish();
+        if (err1 || err2)
+            printf("\nWarning! An error occurred writing WAVE_FILE file.\n");
+    
+    }
+#else // standard output
+    printf("nothing to clean up. . ." );
+    
+#endif
     
 }
 //======================================================================
 //float table[TABLE_LEN]; //arry (table) to be filled with a wave form
 
-void main()
+int main()
 {
     
     
     int waveform;
-    const float frequency, duration;
+    float frequency, duration;
     
     printf("Type the frequency of the wave to output in Hz, and press ENTER: ");
-    scanf("%f", &frequency)
+    scanf("%f", &frequency);
+    
+    printf("\nType the duration of tone in seconds, and press ENTER: ");
+    scanf("%f", &duration);
+    
+    wrong_waveform:
+    printf("\nType a number from 0 to 3 corresponding to the waveform you intend to choose\n");
+    printf("(0 = sine, 1 = square, 2 = sawtooth, 3 = triangle), and press ENTER: ");
+    scanf("%d", &waveform);
+    if (waveform < 0 || waveform > 3)
+    {
+        printf("\nwrong number for waveform, try again!\n");
+        goto wrong_waveform;
+    }
+    
+    //===========fill the table==============================
+    switch (waveform){
+        case SINE:
+            printf("\nYou've chosen a SINE wave\n");
+            fill_sine();
+            break;
+        
+        case SQUARE:
+            printf("\nYou've chosen a SQUARE wave\n");
+            fill_square();
+            break;
+            
+        case SAW:
+            printf("\nYou've chosen a SAW wave\n");
+            fill_saw();
+            break;
+        
+        case TRIANGLE:
+            printf("\nYou've chosen a TRIANGLE wave\n");
+            fill_triangle();
+            break;
+        default: //impossible!
+            printf("\nWrong wave! Ending program. \n");
+            return -1;
+    }
+    
+    
+
+
+init();
+
+
+    int j;
+    double sample_increment = frequency * TABLE_LEN / SAMPLING_RATE;
+    double phase = 0;
+    float sample;
+    
+    for (j = 0; j < duration * SAMPLING_RATE; j++)
+    {
+        sample = table[(long) phase];
+        outSample(sample);
+        phase += sample_increment;
+        if (phase > TABLE_LEN) phase -= TABLE_LEN;
+    }
+    
+
+cleanup();
+printf("End of process\n");
+    return 0;
 }
