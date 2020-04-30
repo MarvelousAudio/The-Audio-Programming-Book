1cbf709cd54edfe8721d4060ad9f7f5c74d7bd97
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-04-29 20:38:54 -0700

    push

---
+++ Player/Player/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Player
+//
+//  Created by ben haywood on 4/25/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include "tinyAudioLib.h"
+#include "portsf.h"
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
