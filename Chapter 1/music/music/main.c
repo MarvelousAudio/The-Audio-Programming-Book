c451486377ee9c6a780f2089c8f2756519b44be1
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-06-12 22:21:37 -0700

    Push
    code works

---
+++ Chapter 1/music/music/main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  music
+
+// 1.1.1 Exercise
+//
+//  Created by ben haywood on 6/12/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    printf("I love music!");
+    
+    return 0;
+}
+
+/*
+ 
+ 
+ exercies 1.1.2
+ 
+ a) io still works
+ b) syntax Error
+ c)nothing happens
+ d) syntax error
+ e) syntax error
+ f) nothing
+ g) syntax error
+ */
