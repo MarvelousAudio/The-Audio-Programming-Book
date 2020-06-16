4c9647897984ad4b60cad92bbf98784fa879b2c4
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-06-15 17:33:13 -0700

    push
    code works

---
+++ Chapter 1/Exercise 1.2.1/Exercise 1.2.1/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Exercise 1.2.1
+//
+//  Created by ben haywood on 6/15/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    printf("size of int: %lu \n", sizeof(int));
+    printf("size of char: %lu \n", sizeof(char));
+    printf("size of short: %lu \n", sizeof(short));
+    printf("size of long: %lu \n", sizeof(long));
+    printf("size of float: %lu \n", sizeof(float));
+    printf("size of double: %lu \n", sizeof(double));
+    
+    return 0;
+}
